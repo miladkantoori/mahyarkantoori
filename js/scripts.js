715e742fa3be2951c479deaa3d65a66d0e91e79e
miladkantoori <46405640+miladkantoori@users.noreply.github.com>
2022-04-25 20:34:59 +0430

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -123,40 +123,6 @@ $(document).ready(function(){
 
     });
 
-    /* Google map
-     ------------------------------------------------*/
-    var map = '';
-    var center;
-
-    function initialize() {
-        var mapOptions = {
-            zoom: 13,
-            center: new google.maps.LatLng(37.779724, -122.452152),
-            scrollwheel: false
-        };
-
-        map = new google.maps.Map(document.getElementById('google-map'),  mapOptions);
-
-        google.maps.event.addDomListener(map, 'idle', function() {
-            calculateCenter();
-        });
-
-        google.maps.event.addDomListener(window, 'resize', function() {
-            map.setCenter(center);
-        });
-    }
-
-    function calculateCenter() {
-        center = map.getCenter();
-    }
-
-    function loadGoogleMap(){
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
-        document.body.appendChild(script);
-    }
-
     // DOM is ready
     $(function() {
         loadGoogleMap(); // Google Map
